b181875b7313f2bc0430e6741dd6d418950a9a31
Antti Kolehmainen <antti@partagames.com>
2020-01-31 23:03:44 +0200

    Added hold for drill

--- Assets/Scripts/ToolManager.cs
+++ Assets/Scripts/ToolManager.cs
@@ -14,10 +14,13 @@ public class ToolManager : MonoBehaviour
         Drill = 1
     }
 
-    [Header("Tool Sprite")]
+    [Header("Visual")]
     public SpriteRenderer ToolSpriteRenderer;
-
     public GameObject[] ToolSprites;
+    [Header("Audio")]
+    public AudioSource AudioSource;
+    public AudioClip[] ToolUseClips;
+    [Header("Settings")]
     public float ToolRadius;
     
     private ToolType SelectedToolType;
@@ -40,9 +43,50 @@ public class ToolManager : MonoBehaviour
         ToolSprites[(int)SelectedToolType].SetActive(true);
     }
 
+    private bool HoldUseSelectedTool(bool holding)
+    {
+        if (SelectedToolType != ToolType.Drill) return false; // only drill is hold
+
+        if (holding == false)
+        {
+            AudioSource.Stop();
+            return true;
+        }
+        
+        if (AudioSource.isPlaying == false)
+        {
+            AudioSource.clip = ToolUseClips[(int) SelectedToolType];
+            AudioSource.loop = true;
+            AudioSource.Play();
+        }
+        
+        var hits = Physics2D.CircleCastAll(transform.position, ToolRadius, Vector2.zero);
+        if (hits.Length > 0)
+        {
+            // first check tooth
+            foreach (RaycastHit2D h in hits)
+            {
+                if (h.collider.CompareTag("Tooth"))
+                {
+                    Debug.Log("Hit a tooth");
+                    h.collider.gameObject.SendMessage("OnUseTool", SelectedToolType, SendMessageOptions.RequireReceiver);
+                    return true; // do not proceed, tooth is prioritized
+                }
+            }
+
+            var firstHit = hits[0];
+            Debug.Log("Hit a " + firstHit.collider.gameObject.name);
+            firstHit.collider.gameObject.SendMessage("OnUseTool", SelectedToolType, SendMessageOptions.DontRequireReceiver);
+        }
+        
+        return true;
+    }
+    
     private void UseSelectedTool()
     {
 
+        if (SelectedToolType == ToolType.Drill) return;
+        
         var hits = Physics2D.CircleCastAll(transform.position, ToolRadius, Vector2.zero);
         if (hits.Length > 0)
         {
@@ -73,10 +117,23 @@ public class ToolManager : MonoBehaviour
             SetTool(ToolType.Drill);
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            UseSelectedTool();
+            bool constantUse = HoldUseSelectedTool(true);
+            if (constantUse == false)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    UseSelectedTool();
+                }
+            }
         }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            HoldUseSelectedTool(false);
+        }
+       
         
         var mousePos = Input.mousePosition;
         var mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
