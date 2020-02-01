987810810168cccb982663b6b110dd7709486118
Antti Kolehmainen <antti@partagames.com>
2020-02-01 20:02:18 +0200

    Intros

--- Assets/Scripts/UI_Ingame.cs
+++ Assets/Scripts/UI_Ingame.cs
@@ -1,29 +1,39 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class UI_Ingame : MonoBehaviour
+public class UI_Ingame : SingletonBehaviour<UI_Ingame>
 {
 
+    public GameObject Intro;
     public GameObject StartMenu;
     public GameObject EndMenu;
 
     private void OnEnable()
     {
-        BeginStartGameSequence();
+        BeginIntroSequence();
+    }
+
+    private void BeginIntroSequence()
+    {
+        Cursor.visible = true;
+        Intro.SetActive(true);
+        StartMenu.SetActive(false);
+        EndMenu.SetActive(false);
     }
 
     public void BeginStartGameSequence()
     {
         Cursor.visible = true;
+        Intro.SetActive(false);
         StartMenu.SetActive(true);
+        EndMenu.SetActive(false);
     }
     
     public void BeginIngameSequence()
     {
         Cursor.visible = false;
+        Intro.SetActive(false);
         StartMenu.SetActive(false);
+        EndMenu.SetActive(false);
     }
 
     public void BeginEndGameSequence()
---
+++ Assets/UI_Intro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Intro : MonoBehaviour
+{
+
+    public void OnIntroEnd()
+    {
+        UI_Ingame.Instance.BeginStartGameSequence();
+    }
+    
+}
