73478818bbaf9fcba8d281c40079a7238460b24c
Antti Kolehmainen <antti@partagames.com>
2020-02-01 22:56:06 +0200

    Teeth are visible at the end

--- Assets/EndMenu.cs
+++ Assets/EndMenu.cs
@@ -4,25 +4,57 @@ public class EndMenu : MonoBehaviour
 {
 
     public Animator Animator;
+    public ParticleSystem SparklesParticle;
+    public ParticleSystem SparklesParticle2;
+    public Vector3 EndToothOffset;
+    public Vector3 EndFrontToothOffset;
 
     private void OnEnable()
     {
+        var allTooths = SetupToothsForUI();
+        
         Animator.ResetTrigger("Goodjob");
         Animator.ResetTrigger("Toobad");
         
-        bool goodJob = CalculateScores();
+        bool goodJob = CalculateScores(allTooths);
         
         if (goodJob) {
+            SparklesParticle.Play();
+            SparklesParticle2.Play();
             Animator.SetTrigger("Goodjob");
             return;
         }
+        SparklesParticle.Stop();
+        SparklesParticle2.Stop();
         Animator.SetTrigger("Toobad");
     }
 
-    private bool CalculateScores()
+    private Tooth[] SetupToothsForUI()
     {
         var allTooths = FindObjectsOfType<Tooth>();
 
+        foreach (var t in allTooths)
+        {
+            var renderers = t.GetComponentsInChildren<SpriteRenderer>();
+            foreach (var r in renderers)
+            {
+                r.sortingLayerName = "UI";
+                r.sortingLayerID = LayerMask.NameToLayer("UI");
+            }
+
+            if (t.gameObject.name.Contains("Top"))
+            {
+                t.transform.position = t.transform.position + EndFrontToothOffset;    
+                continue;
+            }
+            t.transform.position = t.transform.position + EndToothOffset;
+        }
+        
+        return allTooths;
+    }
+
+    private bool CalculateScores(Tooth[] allTooths)
+    {
         int score = 0;
         foreach (var t in allTooths)
         {
