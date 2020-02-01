0b9b3b7cf46199e9d14e4ade607b36e53f927d76
Antti Kolehmainen <antti@partagames.com>
2020-02-01 12:48:32 +0200

    Add random decay

--- Assets/Scripts/Tooth.cs
+++ Assets/Scripts/Tooth.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mime;
 using UnityEngine;
+using Random = System.Random;
 
 public class Tooth : MonoBehaviour
 {
@@ -11,18 +13,45 @@ public class Tooth : MonoBehaviour
         Healthy = 0,
         Cracked = 1,
         Shattered = 2,
-        Broken = 3
+        Decay = 3,
+        Broken = 4
     }
     
+    [Header("Visuals")]
     public Animator Animator;
     public SpriteRenderer ToothSpriteRenderer;
     public Sprite[] ToothConditionSprites;
     public ParticleSystem ToohSpurtParticle;
+    [Header("Settings")]
     public float ToothShatteredTime = 2f;
 
     private ToothCondition toothCondition = ToothCondition.Healthy;
     private float toothShatteredTimeCounter;
-    
+
+    private void OnEnable()
+    {
+        SetToothCondition(toothCondition);
+        
+        var random = UnityEngine.Random.Range(0, 100);
+        if (random < 50)
+        {
+            SetToothCondition(ToothCondition.Decay);    
+        }
+    }
+
+    private void SetToothCondition(ToothCondition toothCondition)
+    {
+        this.toothCondition = toothCondition;
+        
+        if (toothCondition == ToothCondition.Broken)
+        {
+            ToothSpriteRenderer.sprite = null;
+            return;
+        }
+        
+        ToothSpriteRenderer.sprite = ToothConditionSprites[(int)toothCondition];
+    }
+
     public void OnUseTool(ToolManager.ToolType toolType)
     {
         switch (toolType)
@@ -41,29 +70,32 @@ public class Tooth : MonoBehaviour
                 if (toothShatteredTimeCounter >= ToothShatteredTime)
                 {
                     toothShatteredTimeCounter = 0f;
-                    DecayToothCondition();
+                    OnDrillTooth();
                 }
                 break;
         }
         
     }
 
-    private void DecayToothCondition()
+    private void OnDrillTooth()
     {
-        var nextCondition = ((int) toothCondition + 1);
-        if (nextCondition > (int) ToothCondition.Broken)
+        switch (toothCondition)
         {
-            return;
+            case ToothCondition.Healthy:
+                SetToothCondition(ToothCondition.Cracked);
+                break;
+            case ToothCondition.Decay:
+                SetToothCondition(ToothCondition.Cracked);
+                break;
+            case ToothCondition.Cracked:
+                SetToothCondition(ToothCondition.Shattered);
+                break;
+            case ToothCondition.Shattered:
+                SetToothCondition(ToothCondition.Broken);
+                break;
+            case ToothCondition.Broken:
+                // do nothing
+                break;
         }
-
-        toothCondition = (ToothCondition)nextCondition;
-
-        if (toothCondition == ToothCondition.Broken)
-        {
-            ToothSpriteRenderer.sprite = null;
-            return;
-        }
-        
-        ToothSpriteRenderer.sprite = ToothConditionSprites[nextCondition];
     }
 }
