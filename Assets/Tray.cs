da2227f77d4222008b40374fe15bfcf0177a87c6
Antti Kolehmainen <antti@partagames.com>
2020-02-02 07:54:35 +0200

    Fix tray animation

---
+++ Assets/Tray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tray : MonoBehaviour
+{
+
+    public Animator Animator;
+
+    private void Update()
+    {
+        if (UI_Ingame.Instance.MenuActive()) return;
+
+        if (Animator.enabled == false)
+        {
+            Animator.enabled = true;
+        }
+    }
+}
