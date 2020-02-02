801609efecc1323bfafedf23d19b02d4703322e5
Antti Kolehmainen <antti@partagames.com>
2020-02-02 10:35:08 +0200

    Fix orientations

---
+++ Assets/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initializer : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.SetResolution(720, 1280, false);
+        Application.targetFrameRate = 60;
+    }
+}
